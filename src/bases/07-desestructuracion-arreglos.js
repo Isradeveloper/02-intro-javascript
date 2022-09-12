13c68e0f992f80e231ddb0daa6a68b9f33a90f96
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-12 10:57:27 -0500

    07

---
+++ src/bases/07-desestructuracion-arreglos.js
@@ -0,0 +1,25 @@
+const personajes = ['Goku', 'Vegeta', 'Trunks'];
+const [ , , p3 ] = personajes;
+console.log(p3);
+
+const retornaArreglo = ()=> {
+  return ['ABC', 123]
+}
+
+const [letras, numeros] = retornaArreglo();
+console.log(letras, numeros)
+
+const useState = (valor) => {
+  return [valor, ()=> {console.log("Hola mundo")}];
+}
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const [nombre, setNombre] = useState('Goku');
+
+
+// Tarea
+// El primer valor del arreglo se llamará nombre
+// El segundo valor se llamará setNombre()
+
+console.log(nombre)
+setNombre();
