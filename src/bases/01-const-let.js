a9bf87ef80bdb6364211353580d973d7760addfb
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-12 09:28:29 -0500

    01

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+
+// Variables y constantes
+
+const nombre = "Isra";
+const apellido = "Trujillo";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado)
+
+if (true) {
+  let valorDado = 6;
+  
+  console.log(valorDado)
+}
+
+console.log(valorDado)
